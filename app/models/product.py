2c0df608df8f4b8aae0da19a7d7cf75dfa5f4bf5
crvt7 <patryk.popardowski@gmail.com>
2021-06-27 20:36:31 +0200

    podniesienie oceny

--- app/models/product.py
+++ app/models/product.py
@@ -1,3 +1,4 @@
+from os import stat
 from app.models.opinion import Opinion
 import requests
 import json
@@ -7,13 +8,23 @@ class Product:
     def __init__(self, product_id, product_name = None, opinions = []):
         self.product_id = product_id
         self.product_name = product_name
-        self.opinions = opinions
+        self.opinions = opinions.copy()
     
     def extract_product(self):
         next_page = "https://www.ceneo.pl/{}#tab=reviews".format(self.product_id)
         while next_page:
             respons = requests.get(next_page)
             page_dom = BeautifulSoup(respons.text, "html.parser")
+
+            if self.product_name == None:
+                try:
+                    self.product_name = page_dom.find("h1", class_="product-top__product-info__name js_product-h1-link js_product-force-scroll js_searchInGoogleTooltip default-cursor").text[1:]
+                except AttributeError:
+                    try:
+                        self.product_name = page_dom.find("h1", class_="product-top__product-info__name long-name js_product-h1-link js_product-force-scroll js_searchInGoogleTooltip default-cursor").text[1:]
+                    except AttributeError:
+                        self.product_name = "empty_product_name"
+                    
             opinions = page_dom.select("div.js_product-review")
             for opinion in opinions:
                 self.opinions.append(Opinion().extract_opinion(opinion).transform_opinion())
@@ -40,10 +51,34 @@ class Product:
     def save_to_json(self):
         with open(f"app/products/{self.product_id}.json", "w", encoding="UTF-8") as fp:
             json.dump(self.to_dict(), fp, indent=4, ensure_ascii=False)
+        fp.close()
+    
+    def create_json_stats(self):
+        json_data = {}
+        stats = {"id": "N/A", "name" : "N/A", "opinion_count" : 0, "pros_count" : 0, "cons_count" : 0, "avg_score": "Brak ocen"}
+        score = 0
+
+        with open(f"app/products/{self.product_id}.json", "r", encoding="UTF-8") as fp:
+            json_data = json.load(fp)
+
+        stats["id"] = json_data['product_id']
+        stats["name"] = json_data["product_name"]
+        for opinion in json_data["opinions"]:
+            stats["opinion_count"] += 1
+            stats["pros_count"] += len(opinion["pros"])
+            stats["cons_count"] += len(opinion["cons"])
+            score += opinion["stars"]
+        if stats["opinion_count"] > 0:
+            stats["avg_score"] = round((score / stats["opinion_count"]), 2)
+
+        with open(f"app/stats/{self.product_id}_stats.json", "w", encoding="UTF-8") as file:
+            json.dump(stats, file)
 
     def read_from_json(self):
+        print(self.product_id)
         with open(f"app/products/{self.product_id}.json", "r", encoding="UTF-8") as fp:
             prod = json.load(fp)
+        fp.close()
         self.product_name = prod['product_name']
         opinions = prod['opinions']
         for opinion in opinions:
--- app/views.py
+++ app/views.py
@@ -1,28 +1,44 @@
+from flask.helpers import send_file
 from app import app
 from app.models.product import Product
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from os import listdir
+import json
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html.jinja')
 
-@app.route('/extract/<product_id>')
-def extract(product_id):
-    product = Product(product_id)
-    product.extract_product()
-    product.save_to_json()
-    return redirect(url_for('opinions', product_id=product_id))
+@app.route('/extract', methods=['GET', 'POST'])
+def extract():
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        product = Product(product_id)
+        product.extract_product()
+        if product.product_name == "empty_product_name":
+            return render_template('extractError.html.jinja')
+        product.save_to_json()
+        product.create_json_stats()
+        return redirect(url_for('opinions', product_id=product_id))
+    return render_template('extract.html.jinja')
+
 
 @app.route('/products')
 def products():
     products_list = [product.split('.')[0] for product in listdir("app/products")]
-    return render_template('products.html.jinja', products=products_list)
+    stats = {}
+    for file in listdir("app/stats"):
+        with open(f"app/stats/{file}", "r", encoding="UTF-8") as fp:
+            f = json.load(fp)
+            stats[f["id"]] = f
+    return render_template('products.html.jinja', stats=stats, products=products_list)
 
 @app.route('/opinions/<product_id>')
 def opinions(product_id):
+    print(product_id)
     product = Product(product_id)
+    print(", ".join(op.opinion_id for op in product.opinions))
     product.read_from_json()
     return render_template('opinions.html.jinja', product=str(product))
 
@@ -32,4 +48,9 @@ def charts(product_id):
 
 @app.route('/about')
 def about():
-    pass
+    return render_template('about.html.jinja')
+
+@app.route('/download/<file_name>')
+def download_file(file_name):
+    path = app.root_path + "/products/" + file_name + ".json"
+    return send_file(path, as_attachment=True)
